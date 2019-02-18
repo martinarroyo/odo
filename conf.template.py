cd4dcd199a57fc768b18f46431c912a7e81772c0
Diego Martin <diegomartinpi@gmail.com>
2019-02-18 21:36:14 +0100

    mpstat patch

--- conf.template.py
+++ conf.template.py
@@ -4,6 +4,9 @@ DEBUG = True
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_PATH = os.path.join(BASE_DIR, 'build')
 
+REFRESH_INTERVAL = 1000
+PORT = 9000
+
 XHEADERS = True
 
 app_settings = {
