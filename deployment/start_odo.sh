d3dc2b74845162a24e5378338de4fbd14965e79e
Diego Martin <diegomartinpi@gmail.com>
2019-02-19 11:07:28 +0100

    deployment

---
+++ deployment/start_odo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+echo $SCRIPTPATH
+
+. $SCRIPTPATH/../env/bin/activate
+$SCRIPTPATH/../app.py
